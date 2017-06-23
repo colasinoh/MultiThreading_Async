d7f7b7fe7370d129ca71703890334a6e2f1648ce
Harvey Colasino <harvey.colasino@outlook.com>
2017-06-24 02:59:25 +0800

    adding Mocking

--- CountryServiceFactory/Program.cs
+++ CountryServiceFactory/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Moq;
+
 
 namespace CountryServiceFactory
 {
@@ -12,14 +14,34 @@ namespace CountryServiceFactory
         {
             ICountry countryFactory = new ServiceFactory();
 
-            IService service = countryFactory.CreateService(CountrySpecific.Japan);
-            service.CalcDiscount();
+            IService service = countryFactory.CreateService(CountrySpecific.Other);
+            Console.WriteLine(service.CalcDiscount());
 
-            IService serviceJapan = countryFactory.CreateService(CountrySpecific.SouthKorea);
-            serviceJapan.CalcDiscount();
+            //test Other Countries Service
+            var mockServiceOther = new Mock<Country>();
+            mockServiceOther.Setup(m => m.CalcDiscount()).Returns(() => "Here goes the general calculation for other countries...");
+            Console.WriteLine("Mock Country: " + mockServiceOther.Object.CalcDiscount());
 
-            IService serviceSouthKorea = countryFactory.CreateService(CountrySpecific.Other);
-            serviceSouthKorea.CalcDiscount();
+            IService serviceJapan = countryFactory.CreateService(CountrySpecific.Japan);
+            Console.WriteLine(serviceJapan.CalcDiscount());
+
+            //test Japan Service
+            var mockServiceJapan = new Mock<Country>();
+            mockServiceJapan.Setup(m => m.CalcDiscount()).Returns(() => "Here goes the calculation specific to Japan...");
+            Console.WriteLine("Mock Japan: " + mockServiceJapan.Object.CalcDiscount());
+
+            IService serviceSouthKorea = countryFactory.CreateService(CountrySpecific.SouthKorea);
+            Console.WriteLine(serviceSouthKorea.CalcDiscount());
+
+            //test South Korea Service
+            var mockServiceKorea = new Mock<Country>();
+            mockServiceKorea.Setup(m => m.CalcDiscount()).Returns(() => "Here goes the calculation specific to South Korea...");
+            Console.WriteLine("Mock Korea: " + mockServiceKorea.Object.CalcDiscount());
+
+            //implement multiple interface in Service mock
+            //var serviceMock = new Mock<IService>();
+            //var disposableService = serviceMock.As<IDisposable>();
+            //disposableService.Setup(dS => dS.Dispose());
 
             Console.ReadKey();
         }
@@ -34,30 +56,30 @@ namespace CountryServiceFactory
 
     public interface IService
     {
-        void CalcDiscount();
+        string CalcDiscount();
     }
 
     public class CountryJapan : IService
     {
-        public void CalcDiscount()
+        public virtual string CalcDiscount()
         {
-            Console.WriteLine("Here goes the calculation specific to Japan...");
+            return "Here goes the calculation specific to Japan...";
         }
     }
 
     public class CountrySouthKorea : IService
     {
-        public void CalcDiscount()
+        public virtual string CalcDiscount()
         {
-            Console.WriteLine("Here goes the calculation specific to South Korea...");
+            return "Here goes the calculation specific to South Korea...";
         }
     }
 
     public class Country : IService
     {
-        public void CalcDiscount()
+        public virtual string CalcDiscount()
         {
-            Console.WriteLine("Here goes the general calculation for other countries...");
+            return "Here goes the general calculation for other countries...";
         }
     }
 
